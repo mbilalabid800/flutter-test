5017a1428c1b04271823d107df32e20bcbdcd0e1
Muhammad Bilal <mbilalabid800@gmail.com>
2025-04-13 14:20:10 +0500

    Add splash screen feature

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color screenBackground = Color.fromRGBO(255, 255, 255, 1);
+  static const Color textColor = Color.fromRGBO(0, 0, 0, 1);
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,96 @@
+import 'package:cartzilla/constants/app_colors.dart';
+import 'package:cartzilla/responsive/device_dimensions.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppColors.screenBackground,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: DeviceDimensions.screenHeight(context) * 0.010),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Image.asset(
+                  'assets/images/splash-image.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+              SizedBox(height: DeviceDimensions.screenHeight(context) * 0.010),
+              Text(
+                'Find The\nBest Collections',
+                style: GoogleFonts.inter(
+                  fontSize: DeviceDimensions.responsiveSize(context) * 0.090,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.textColor,
+                ),
+              ),
+
+              Text(
+                'Get your dream item easily with FashionHub and get other intersting offer',
+                style: GoogleFonts.inter(fontSize: 14, color: Colors.black54),
+              ),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal:
+                            DeviceDimensions.screenWidth(context) * 0.125,
+                      ),
+                      side: const BorderSide(color: Colors.black12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: Text(
+                      'Sign up',
+                      style: GoogleFonts.inter(
+                        fontSize: 16,
+                        color: AppColors.textColor,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black87,
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal:
+                            DeviceDimensions.screenWidth(context) * 0.125,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: Text(
+                      'Sign in',
+                      style: GoogleFonts.inter(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: DeviceDimensions.screenHeight(context) * 0.010),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
